097331747ec672bd52919fda6852e8dcbd31d85b
Micah J. Obal <micah_obal@yahoo.com>
2015-03-26 00:12:20 -0500

    Finished 003E. It's sloppy and can't decode yet, but I might add that functionality later.

--- 003 Easy - Alphabet Cypher/main.cpp
+++ 003 Easy - Alphabet Cypher/main.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
-#include <fstream>
+#include <algorithm>
 #include <string>
 using namespace std;
 
 int main(){
 	string unencodedWord;
 	bool matchFound = false;
-	int alphabetCounter = 0;
-	char alphabet = ['A', 'B', 'C' ,'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+	int alphabetCounter = 0, cypher = 5;
+	char alphabet[] = {'A', 'B', 'C' ,'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	
 	cout<<"Please input a word to be encoded."<<endl;
 	cin>>unencodedWord;
+	cout<<unencodedWord<<" has a length of "<<unencodedWord.length()<<endl;
+	
+	
 	for (int i = 0; i < unencodedWord.length(); i += 1) {
 		alphabetCounter = 0;
-		while (!matchFound || alphabetCounter >= alphabet.length()) }
-			if (alphabet[alphabetCounter] == unencodedWord[i]){
-				unencodedWord[i] = alphabet[(i + 1) % alphabet.length()]
+		matchFound = false;
+		unencodedWord[i] = toupper(unencodedWord[i]);
+		while ((!matchFound) && !(alphabetCounter > 25)) {
+			if (alphabet[alphabetCounter] == unencodedWord[i]) {
+				unencodedWord[i] = alphabet[(alphabetCounter + cypher) % 26];
 				matchFound = true;
-			}
+				cout<<"Letter found. i = "<<i<<". alphabetCounter = "<<alphabetCounter<<endl;
+			};
 			alphabetCounter += 1;
-		}
-		unencodedWord[i] 
-	}
+		};
+	};
+	
 	cout<<"Your newly encoded word: "<<unencodedWord<<endl;
-}
+};
