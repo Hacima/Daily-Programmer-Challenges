c9af582aef9e6a9eec0cbd9c8261371552b5ca56
Micah J. Obal <micah_obal@yahoo.com>
2015-03-31 21:19:57 -0500

    Started 207E...

---
+++ 208 Easy - Culling Numbers/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string numbers = "1 1 2 2 3 3 4 4";
+	string results;
+	
+	for (int i = 0; i < numbers.length(); i += 1) {
+		/*iterate through the numbers string, adding unique numbers to the results string
+		comapre each number to all the numbers in the results string and ignore all matches
+		find a way to only include characters between blank spaces...*/
+	}
+	
+}
