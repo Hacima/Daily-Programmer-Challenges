6158749d53abbf0ca3c01b6b2a143453aaf85d0d
Micah J. Obal <micah_obal@yahoo.com>
2015-04-01 21:30:39 -0500

    Making slow progress. Really just going through the motions. Motivation is a fickle thing.

--- 208 Easy - Culling Numbers/main.cpp
+++ 208 Easy - Culling Numbers/main.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 int main() {
 	string numbers = "1 1 2 2 3 3 4 4";
-	string results;
+	string results, placeholder;
 	
-	for (int i = 0; i < numbers.length(); i += 1) {
-		/*iterate through the numbers string, adding unique numbers to the results string
-		comapre each number to all the numbers in the results string and ignore all matches
-		find a way to only include characters between blank spaces...*/
+	for (string::size_type i = 0; i < numbers.length(); i += 1) {
+		if (numbers[i] == " " || i = (numbers.length() - 1)){
+			results += placeholder;
+			placeholder = "";
+		} else {
+			placeholder += numbers[i];
+		}
 	}
 	
 }
