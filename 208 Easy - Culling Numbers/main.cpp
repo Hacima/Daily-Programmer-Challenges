cfac25e274fd37dd2e030636fef3e658839fe4d4
Micah J. Obal <micah_obal@yahoo.com>
2015-04-02 22:59:40 -0500

    Finished 208E. Was a little trickier than I thought, but I learned more about string manipulation.

--- 208 Easy - Culling Numbers/main.cpp
+++ 208 Easy - Culling Numbers/main.cpp
@@ -5,13 +5,20 @@ int main() {
 	string numbers = "1 1 2 2 3 3 4 4";
 	string results, placeholder;
 	
-	for (string::size_type i = 0; i < numbers.length(); i += 1) {
-		if (numbers[i] == " " || i = (numbers.length() - 1)){
-			results += placeholder;
-			placeholder = "";
+	for (string::size_type i = 0; i < numbers.length(); i++) {
+		if ((numbers[i] == ' ')|| i == (numbers.length() - 1)) {
+			if (results.find(placeholder) != string::npos) {
+				placeholder = "";
+			} else {
+				results += placeholder;
+				placeholder = "";
+			}
 		} else {
 			placeholder += numbers[i];
 		}
 	}
+	
+	cout<<"Original: "<<numbers<<endl;
+	cout<<"Culled: "<<results<<endl;
 	
 }
